f4c4a2c47e76ff4089b6de1f560921c29401d624
Xilaeth <ger.fra.dr.felix@gmail.com>
2023-07-10 18:00:26 +0200

    day 1.1

--- Crank-Nicholson/nicholson.py
+++ Crank-Nicholson/nicholson.py
@@ -1,51 +1,56 @@
 import numpy as np
+from numpy import linalg
 import matplotlib.pyplot as plt
 import matplotlib.animation as ani
 
-def gauss_seidel(A, b, k):
+def gauss_seidel(A, b, limit):
     size = b.shape[0]
-    x = np.zeros(size, float)
-    for i in range(k):
-        xn = np.zeros(size, float)
-        for i in range(A.shape[0]):
-            a1 = np.dot(A[i, :i], xn[:i])
-            a2 = np.dot(A[i, i+1 :], x[i+1 :])
-            xn[i] = (b[i] - a1 - a2) / A[i, i]
-            x = xn
+    x = np.ones(size, float)
+    for i in range(limit):
+        xn = np.ones(size, float)
+        for j in range(size):
+            a1 = 0
+            a2 = 0
+            for k in range(j):
+                a1 += A[j, k] * xn[k]
+            for k in range(j+1, size):
+                a2 += A[j, k] * x[k]
+            xn[j] = (b[j] - a1 - a2) / A[j, j]
+        if linalg.norm(x - xn) < 1e-10:
+            print(i, "|", linalg.norm(x - xn))
+            break            
+        if i == limit-1:
+            print("reached limit")
+            exit()
+        x = xn
     return x
 
-def const_M(a1, a2, a3, size):
-    v1 = np.full(size, a1)
-    v2 = np.full(size-1, a2)
-    v3 = np.full(size-1, a3)
-    return np.diag(v2, -1) + np.diag(v1, 0) + np.diag(v3, 1)
+def gauss_seidel_tri(A, b, a1, a2, a3, limit):
+    size = b.shape[0]
+    x = np.zeros(size, float)
+    for i in range(limit):
+        xn = np.ones(size, float)
+        for j in range(size):
+            a = 0
+            if j == 0:
+                a += a3*x[j+1]
+            elif j == size-1:
+                a += a1*x[j-1]
+            else:
+                a += a1*x[j-1] + a3*x[j+1]
+            xn[j] = (b[j] - a) / a2
+        if linalg.norm(x - xn) < 1e-10:
+            print(i, "|", linalg.norm(x - xn))
+            break
+        if i == limit-1:
+            print("reached limit")
+            exit()
+        x = xn
+    return x
 
-a = 1
-dx = 0.1
-dt = 0.1
-
-r = (a*dt)/(2* dx**2)
-
-a1 = (1 + 2*r)
-a2 = -r
-a3 = -r
-size = 10
-
-M = const_M(a1, a2, a3, 100)
-
-f1 = np.zeros(100)
-f1[0] = 1
-t = np.linspace(0, 100*dx, 100)
-
-fig, ax = plt.subplot()
-line, = ax.plot(t, f1)
-ax.grid()
-
-def gen_data(   
-
-for i in range(5):
-    plt.plot(t, f1)
-    x = gauss_seidel(M, f1, 1000)
-    f1 = x
-
-plt.show()
+tn = 100
+xn = 100
+x = 1
+t = 10
+dt = int(t / tn)
+dx = int(x / xn)
--- Gauss-Seidel/gauss.py
+++ Gauss-Seidel/gauss.py
@@ -1,21 +1,41 @@
 import numpy as np
+from numpy import linalg
 
-A = np.array([[2, 3], [-1, 4]])
-b = np.array([1, 0])
+A = np.array([[3, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]], float)
+b = np.array([1, 2, 3], float)
 
-def gauss_seidel(A, b, k):
+# a1 = np.dot(A[j, :j-1], xn[:j-1])
+# a2 = np.dot(A[j, j:], x[j:])
+# print("A: ",A[j, j+1:])
+# print("x: ",x[j+1:])
+# xn[j] = (b[j] - a1 - a2) / A[j, j]
+
+def gauss_seidel(A, b, limit):
     size = b.shape[0]
-    x = np.zeros(size, float)
-    for i in range(k):
-        xn = np.zeros(size, float)
-        for i in range(A.shape[0]):
-            a1 = np.dot(A[i, :i], xn[:i])
-            a2 = np.dot(A[i, i+1 :], x[i+1 :])
-            xn[i] = (b[i] - a1 - a2) / A[i, i]
-            x = xn
+    x = np.ones(size, float)
+    for i in range(limit):
+        xn = np.ones(size, float)
+        for j in range(size):
+            a1 = 0
+            a2 = 0
+            for k in range(j):
+                a1 += A[j, k] * xn[k]
+            for k in range(j+1, size):
+                a2 += A[j, k] * x[k]
+            xn[j] = (b[j] - a1 - a2) / A[j, j]
+        if linalg.norm(x - xn) < 1e-10:
+            print(i, "|", linalg.norm(x - xn))
+            break            
+        if i == limit-1:
+            print("reached limit")
+            exit()
+        x = xn
     return x
 
-v = gauss_seidel(A, b, 1000)
-print(b)
+v = gauss_seidel(A, b, 100)
+A2 = linalg.inv(A)
+#print(linalg.det(A))
 print(v)
-print(np.matmul(A, v))
+print(np.matmul(A2, b))

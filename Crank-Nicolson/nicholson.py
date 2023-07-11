cc70e7a1148f162f6b4f12a949c8cd65f347cc26
Xilaeth <ger.fra.dr.felix@gmail.com>
2023-07-11 16:10:14 +0200

    day 2.4

--- Crank-Nicolson/nicholson.py
+++ Crank-Nicolson/nicholson.py
@@ -2,6 +2,7 @@ import numpy as np
 from numpy import linalg
 import matplotlib.pyplot as plt
 import matplotlib.animation as anim
+import matplotlib.patches as mpatches
 
 
 # the matrix has to be strictly diagonally dominant, meaning a1 + a3 < a2 OR positive definite and symmetric
@@ -13,7 +14,7 @@ def gauss_seidel_tri(b, a1, a2, a3, b1, b2, b3, pot, limit):
         for j in range(1, size-1):
             a = 0
             a += a1*xn[j-1] + a3*x[j+1]
-            xn[j] = ((b1*b[j-1] + b2*b[j] + b3*b[j+1]) - a) / a2
+            xn[j] = ((b1*b[j-1] + (b2 + (dt/2)*pot(dx*j))*b[j] + b3*b[j+1]) - a) / (a2 - (dt/2)*pot(dx*j))
         if linalg.norm(x - xn) < 1e-8:
             print(i, "|", linalg.norm(x - xn))
             break
@@ -53,7 +54,7 @@ v = np.array(list(map(lambda y: np.e ** (-(((dx*y)-mean)**2)/sigma + 1j*k*(dx*y)
 v[0] = 0.0
 v[xn-1] = 0.0
 print(v)
-t = np.linspace(0, x, xn)
+x_range = np.linspace(0, x, xn)
 
 a = 1j/hbar
 b = (dt*(hbar**2))/(4*m*(dx**2))
@@ -62,14 +63,13 @@ b1, b2, b3 = a*b, (1-2*a*b), a*b
 print(a)
 
 def V(x):
-    return a*x
-
-# plt.plot(t, np.absolute(v), "r")
-# v2 = gauss_seidel_tri(v, a1, a2, a3, b1, b2, b3, V, limit)
-# print(v2)
-# plt.plot(t, np.absolute(v2), "g")
+    if x > 0.7 and x < 0.72:
+        return -50000.0
+    else:
+        return 0.0
 
 skip = 5
+interval = 20
 
 vs = []
 for i in range(tn):
@@ -78,11 +78,14 @@ for i in range(tn):
     v = gauss_seidel_tri(v, a1, a2, a3, b1, b2, b3, V, limit)
 
 def animation(data):
-    line.set_xdata(t)
+    line.set_xdata(x_range)
     line.set_ydata(np.absolute(vs[data]))
     return line,
 
-ani = anim.FuncAnimation(fig, animation, frames=(tn//skip), interval=20, blit=True)
-ani.save("wave_packet.mp4", fps=30, extra_args=['-vcodec', 'libx264'])
+wall = mpatches.Rectangle((0.7, 0.0), 0.03, 1.2, fill= False, color = "black", linewidth=0.5)
+fig.gca().add_patch(wall)
 
-# plt.show()
+ani = anim.FuncAnimation(fig, animation, frames=(tn//skip), interval=interval, blit=True)
+# ani.save("quantum_tunneling.mp4", fps=30, extra_args=['-vcodec', 'libx264'])
+
+plt.show()

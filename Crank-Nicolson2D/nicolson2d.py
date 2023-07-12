5d2d62560599f29dedc9fc2f1bf9379c04a85670
Xilaeth <ger.fra.dr.felix@gmail.com>
2023-07-12 16:54:43 +0200

    day 3.3

--- Crank-Nicolson2D/nicolson2d.py
+++ Crank-Nicolson2D/nicolson2d.py
@@ -2,18 +2,20 @@ import numpy as np
 from numpy import linalg
 import matplotlib.pyplot as plt
 import matplotlib.animation as anim
-import matplotlib.patches as mpatches
 
 # v is an NxN Matrix
-def gauss_seidel_tri_vec(b, l, limit):
-    size = b.shape[0]
-    vec = np.zeros_like(b, complex)
-    a = np.zeros_like(b, complex)
+def gauss_seidel_tri_vec(v, l, limit):
+    size = v.shape[0]
+    vec = np.zeros_like(v, complex)
+    a = np.zeros_like(v, complex)
+    b = np.zeros_like(v, complex)
     for rep in range(limit):
-        vec_new = np.zeros_like(b, complex)
+        vec_new = np.zeros_like(v, complex)
         # calculating A-matrix
         for i in range(1, size-1):
             for j in range(1, size-1):
+                if rep == 0:
+                    b[i, j] = (l/2) * (v[i-1, j] + v[i+1, j] + v[i, j-1] + v[i, j+1]) + (1 - 2*l) * v[i, j]
                 a[i, j] = (l/2) * (vec_new[i-1, j] + vec_new[i, j-1] + vec[i+1, j] + vec[i, j+1])
         vec_new = (b - a) / (1 + 2*l)
         if linalg.norm(vec - vec_new) < 1e-10:
@@ -25,14 +27,17 @@ def gauss_seidel_tri_vec(b, l, limit):
         vec = vec_new
     return vec
 
-def gauss_seidel_tri(b, l, limit):
-    size = b.shape[0]
-    vec = np.zeros_like(b, complex)
+def gauss_seidel_tri(v, l, limit):
+    size = v.shape[0]
+    vec = np.zeros_like(v, complex)
+    b = np.zeros_like(v, complex)
     for rep in range(limit):
-        vec_new = np.zeros_like(b, complex)
-        # calculating A-matrix
+        vec_new = np.zeros_like(v, complex)
         for i in range(1, size-1):
             for j in range(1, size-1):
+                # calculating the b-matrix
+                if rep == 0:
+                    b[i, j] = (l/2) * (v[i-1, j] + v[i+1, j] + v[i, j-1] + v[i, j+1]) + (1 - 2*l) * v[i, j]
                 a = (l/2) * (vec_new[i-1, j] + vec_new[i, j-1] + vec[i+1, j] + vec[i, j+1])
                 vec_new[i, j] = (b[i, j] - a) / (1 + 2*l)
         if linalg.norm(vec - vec_new) < 1e-10:
@@ -55,53 +60,49 @@ def inhom(v, l):
     
 
 limit = 1000
-tn = 1000
-xn = 50
-yn = 50
+tn = 25000
+xn = 200
+yn = 200
 x = 1
 y = 1
-t = 0.0004
+t = 0.004
 dt = complex(t / tn)
 dx = complex(x / xn)
 dy = complex(y / yn)
 m = 1
 hbar = 1
 k = np.array([500, 500], complex)
-print(k)
 
 fig, ax = plt.subplots()
 
-sigma = 0.005
-mean_x = 0.5
-mean_y = 0.5
+sigma = x/100
+mean_x = x/2
+mean_y = x/2
 
 lamb = 1j*((dt*hbar)/(2*m*(dx**2)))
 
-v0 = np.array(list(map(lambda x0: list(map(lambda y0: np.e ** ((-( ((dx*x0 - mean_x)**2 + (dy*y0 - mean_y)**2)/sigma)) + 1j*np.dot(k, [dx*x0, dy*y0])), range(yn))), range(xn))), complex)
+v0 = np.array(list(map(lambda x0: list(map(lambda y0: np.e ** ((-( ((dx*x0 - mean_x)**2 + (dy*y0 - mean_y)**2)/sigma)) + 1j*np.dot(k, np.array([dx*x0, dy*y0], complex))), range(yn))), range(xn))), complex)
 
+# plt.matshow(np.imag(v0))
+
+mat = ax.matshow(np.absolute(v0))
 
 skip = 5
-interval = 20
+interval = 40
 
 vs = []
 for i in range(tn):
     if i % skip == 0:
         vs.append(v0)
-    v0 = gauss_seidel_tri(inhom(v0, lamb), lamb, limit)
+    v0 = gauss_seidel_tri_vec(v0, lamb, limit)
     s1 = np.array(sum(np.absolute(v0)**2))
     print(i, " | ", sum(s1), "\n----")
 
-# dif = vs[0] - vs[len(vs)-1]
-# print(dif)
-# print(sum(sum(dif)))
+def animation(data):
+    mat.set_data(np.absolute(vs[data])**2)
+    return mat,
 
-# mat.set_data(np.absolute(vs[len(vs)-1]**2))
-
-# def animation(data):
-    # mat.set_matrix(np.absolute((vs[data])**2))
-    # return mat,
-
-# ani = anim.FuncAnimation(fig, animation, frames=(tn//skip), interval=interval, blit=True)
-# ani.save("quantum2d.mp4", fps=30, extra_args=['-vcodec', 'libx264'])
+ani = anim.FuncAnimation(fig, animation, frames=(tn//skip), interval=interval, blit=True)
+# ani.save("quantum2d-e-7.mp4", fps=30, extra_args=['-vcodec', 'libx264'])
 
 plt.show()

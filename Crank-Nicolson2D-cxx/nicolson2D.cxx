ebf9382bc14b144d9d0544ee7d87eff3e1157152
Xilaeth <ger.fra.dr.felix@gmail.com>
2023-07-14 15:23:27 +0200

    day 5.4

--- Crank-Nicolson2D-cxx/nicolson2D.cxx
+++ Crank-Nicolson2D-cxx/nicolson2D.cxx
@@ -14,9 +14,9 @@ using namespace std;
 
 const int limit = 1000; 
 const int time_steps = 1000;
-const int x_steps = 400;
+const int x_steps = 500;
 const CLD x_len = 1.0;
-const CLD t_len = 0.006;
+const CLD t_len = 0.004;
 const CLD kx = 0;
 const CLD ky = 500;
 const CLD dt = t_len / (CLD)time_steps;
--- Crank-Nicolson2D-cxx/plot_vispy.py
+++ Crank-Nicolson2D-cxx/plot_vispy.py
@@ -1,5 +1,5 @@
 import numpy as np
-from vispy import app, scene
+from vispy import app, scene, io, color
 
 file = open("nicolson2d.txt", "r")
 dat = file.read()
@@ -32,7 +32,7 @@ sepsep = np.array(sepsep)
 size = len(sepsep[0])
 tim = len(sepsep)
 interval = 20
-size = size
+sepsep *= size
 
 canvas = scene.SceneCanvas(keys='interactive', size=(size, size), show=True)
 view = canvas.central_widget.add_view()
@@ -42,21 +42,31 @@ dx, dy = 1/size, 1/size
 x = np.linspace(0, size, size)
 y = np.linspace(0, size, size)
 x, y = np.meshgrid(x, y)
-z = sepsep[0]
+Z = sepsep[0]
 
-surface = scene.visuals.SurfacePlot(x=x, y=y, z=z, shading='smooth')
+surface = scene.visuals.SurfacePlot(x=x, y=y, z=Z, shading='smooth')
+
+# c = color.get_colormap("hsl").map(Z/np.max(Z)).reshape(Z.shape + (-1,))
+# c = c.flatten().tolist()
+# c = list(map(lambda x,y,z,w:(x,y,z,w), c[0::4],c[1::4],c[2::4],c[3::4]))
+
+# surface.mesh_data.set_vertex_colors(c)
+
 view.add(surface)
+
+view.camera = 'turntable'
 
 count = 0
 
 def update(event):
-    global count
+    global count, colormap
     new_z = sepsep[count]
     surface.set_data(z=new_z)
+    frame = canvas.render()
+    io.write_png(f'vispy-frames/frame-{count:03d}.png', frame)
     count += 1
-
     canvas.update()
 
-timer = app.Timer(connect=update, start=True)
+timer = app.Timer(connect=update, start=1/15)
 
 app.run()

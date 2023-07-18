f22dcb63590109f19cf2a368e13a20ad4c1c5f50
Xilaeth <ger.fra.dr.felix@gmail.com>
2023-07-18 16:17:55 +0200

    day 7

--- Crank-Nicolson2D-cxx/nicolson2D.cxx
+++ Crank-Nicolson2D-cxx/nicolson2D.cxx
@@ -14,10 +14,10 @@
 using namespace std;
 
 const int limit = 1000; 
-const int time_steps = 1000;
+const int time_steps = 6000;
 const int x_steps = 400;
 const CLD x_len = 1.0;
-const CLD t_len = 0.0025;
+const CLD t_len = 0.03;
 const CLD kx = 0;
 const CLD ky = 500;
 const CLD dt = t_len / (CLD)time_steps;
@@ -31,7 +31,7 @@ const CLD l = (CLD)1i * dt / ((CLD)4*dx*dx);
 const CLD pot_fac = (CLD)(1i)*(dt/(CLD)2.0);
 
 const CLD V0 = (ky*ky)/(CLD)2.0;
-const CLD omega = (CLD)0.1 * sigma;
+const CLD omega = (CLD)0.025 * sigma;
 
 const double wall_2_y = 0.43;
 const double wall_2_x1 = 0.470;
@@ -66,42 +66,6 @@ void gauss_seidel_tri(EMAT &vec) {
     b.resize(x_steps, x_steps);
     b.setZero();
 
-    //EMAT rands;
-    //int rand_numbs = 10;
-    //rands.resize(rand_numbs, 2);
-    //rands.setZero();
-
-    //for (int i = 0; i < rand_numbs; i++) {
-        //rands(i, 0) = (rand() % x_steps); 
-        //rands(i, 1) = (rand() % x_steps); 
-    //}
-
-    ////cout << rands << endl;
-
-    //int s = 0;
-    //for (int ix = 1; ix < x_steps-1; ix++) {
-        //for (int iy = 1; iy < x_steps-1; iy++) {
-            //for (int in = 0; in < rand_numbs; in++) {
-                //if ((CLD)ix == rands(in, 0) && (CLD)iy == rands(in, 1)) {
-                    //s = 1;
-                //}
-            //}
-            //if (s == 1) {
-                //CLD temp_mean_x = (CLD)ix*dx;
-                //CLD temp_mean_y = (CLD)iy*dx;
-                //for (int i = 1; i < x_steps-1; i++) {
-                    //for (int j = 1; j < x_steps-1; j++) {
-                        //temp(i, j) = pot_fac*V0*exp((-(pow(((CLD)i*dx - temp_mean_x), 2) + pow((CLD)j*dx - temp_mean_y, 2))/omega));
-                    //}
-                //}
-                //pot = pot + temp;
-            //} else {
-                //continue;
-            //}
-            //s = 0;
-        //}
-    //}
-
     for (int ix = 1; ix < x_steps-1; ix++) {
         for (int iy = 1; iy < x_steps-1; iy++) {
             //pot(ix, iy) = potential((CLD)ix*dx, (CLD)iy*dx);
@@ -113,16 +77,16 @@ void gauss_seidel_tri(EMAT &vec) {
         new_vec.setZero();
         for (int ix = 1; ix < x_steps-1; ix++) {
             for (int iy = 1; iy < x_steps-1; iy++) {
-                if (real((CLD)iy*dx) > wall_y && real((CLD)iy*dx) < (wall_y + wall_width)) {
-                    if (real((CLD)ix*dx) < wall_x1 || real((CLD)ix*dx) > wall_x4 || (real((CLD)ix*dx) > wall_x2 && real((CLD)ix*dx) < wall_x3)) {
-                        continue;
-                    }
-                } 
-                if (real((CLD)iy*dx) > wall_2_y && real((CLD)iy*dx) < (wall_2_y + wall_2_width)) {
-                    if (real((CLD)ix*dx) < wall_2_x1 || real((CLD)ix*dx) > wall_2_x2) {
-                        continue;
-                    }
-                } 
+                //if (real((CLD)iy*dx) > wall_y && real((CLD)iy*dx) < (wall_y + wall_width)) {
+                    //if (real((CLD)ix*dx) < wall_x1 || real((CLD)ix*dx) > wall_x4 || (real((CLD)ix*dx) > wall_x2 && real((CLD)ix*dx) < wall_x3)) {
+                        //continue;
+                    //}
+                //} 
+                //if (real((CLD)iy*dx) > wall_2_y && real((CLD)iy*dx) < (wall_2_y + wall_2_width)) {
+                    //if (real((CLD)ix*dx) < wall_2_x1 || real((CLD)ix*dx) > wall_2_x2) {
+                        //continue;
+                    //}
+                //} 
                 a = -l * (new_vec(ix-1, iy) + vec(ix+1, iy) + new_vec(ix, iy-1) + vec(ix, iy+1));
                 new_vec(ix, iy) = (b(ix, iy) - a) / ( (CLD)1 + (CLD)4*l - pot(ix, iy));
             }
@@ -155,54 +119,51 @@ int main() {
         }
     }
 
-    //EMAT rands;
-    //int rand_numbs = 10;
-    //rands.resize(rand_numbs, 2);
-    //rands.setZero();
+    EMAT rands;
+    int rand_numbs = 50;
+    rands.resize(rand_numbs, 2);
+    rands.setZero();
 
-    //for (int i = 0; i < rand_numbs; i++) {
-        //rands(i, 0) = (rand() % x_steps); 
-        //rands(i, 1) = (rand() % x_steps); 
-    //}
+    for (int i = 0; i < rand_numbs; i++) {
+        rands(i, 0) = (rand() % x_steps); 
+        rands(i, 1) = (rand() % x_steps); 
+    }
 
-    //EMAT temp;
-    //temp.resize(x_steps, x_steps);
-    //temp.setZero();
+    EMAT temp;
+    temp.resize(x_steps, x_steps);
+    temp.setZero();
 
     pot.resize(x_steps, x_steps);
     pot.setZero();
 
-    //int s = 0;
-    //for (int ix = 1; ix < x_steps-1; ix++) {
-        //for (int iy = 1; iy < x_steps-1; iy++) {
-            //for (int in = 0; in < rand_numbs; in++) {
-                //if ((CLD)ix == rands(in, 0) && (CLD)iy == rands(in, 1)) {
-                    //s = 1;
-                //}
-            //}
-            //if (s == 1) {
-                //CLD temp_mean_x = (CLD)ix*dx;
-                //CLD temp_mean_y = (CLD)iy*dx;
-                //for (int i = 1; i < x_steps-1; i++) {
-                    //for (int j = 1; j < x_steps-1; j++) {
-                        //temp(i, j) = pot_fac*V0*exp((-(pow(((CLD)i*dx - temp_mean_x), 2) + pow((CLD)j*dx - temp_mean_y, 2))/omega));
-                    //}
-                //}
-                //pot = pot + temp;
-            //} else {
-                //continue;
-            //}
-            //s = 0;
-        //}
-    //}
-
-    //cout << pot << endl;
-    //exit(0);
+    int s = 0;
+    for (int ix = 1; ix < x_steps-1; ix++) {
+        for (int iy = 1; iy < x_steps-1; iy++) {
+            for (int in = 0; in < rand_numbs; in++) {
+                if ((CLD)ix == rands(in, 0) && (CLD)iy == rands(in, 1)) {
+                    s = 1;
+                }
+            }
+            if (s == 1) {
+                CLD temp_mean_x = (CLD)ix*dx;
+                CLD temp_mean_y = (CLD)iy*dx;
+                for (int i = 1; i < x_steps-1; i++) {
+                    for (int j = 1; j < x_steps-1; j++) {
+                        temp(i, j) = pot_fac*V0*exp((-(pow(((CLD)i*dx - temp_mean_x), 2) + pow((CLD)j*dx - temp_mean_y, 2))/omega));
+                    }
+                }
+                pot = pot + temp;
+            } else {
+                continue;
+            }
+            s = 0;
+        }
+    }
 
     ofstream dat;
     ofstream dat2;
-    dat.open("nicolson2d-2.txt");
-    dat2.open("nicolson2d-slice-2.txt");
+    dat.open("nicolson2d-rand-50.txt");
+    dat2.open("nicolson2d-slice-rand-50.txt");
     EMAT v0;
     v0 = vec;
 
